3545d6478739e9cb3dc9b114b0e70c80f64cbc4e
lihaibo <huiji>
2021-04-20 18:18:27 +0800

    [add] 订单80服务初始化

---
+++ cloud-comsumer-order80/src/test/java/com/lhb/springcloud/OrderMain80Tests.java
@@ -0,0 +1,13 @@
+package com.lhb.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMain80Tests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
